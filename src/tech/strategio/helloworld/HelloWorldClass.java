42dcbd6caa90512a363f87aee8dc381967499418
Ernie Hsiung <e@erniehsiung.com>
2022-08-19 14:16:32 -0400

    build: add boilerplate

---
+++ src/tech/strategio/helloworld/HelloWorldClass.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package tech.strategio.helloworld;
+
+/**
+ * @author erniehsiung
+ *
+ */
+public class HelloWorldClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("look at me, I am on fire");
+	}
+
+}
